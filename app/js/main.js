00ccfc7d122c5d9f8319d67c1069bd0f3aac837d
erikotomita <tomita_e@allgrow.co.jp>
2022-06-22 00:30:49 +0900

    色々追加した　

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+$(function () {
+  $(".slider").slick({
+    autoplay: true,
+    adaptiveHeight: true,
+    prevArrow: '<button class="prev-arrow"></button>',
+    nextArrow: '<button class="next-arrow"></button>'
+  });
+});
